d9fbbe40627760e6cc35dde6f24ab6801feca932
harrison-fernandez <hfernandez8297@gmail.com>
2017-06-03 11:13:29 -0700

    Add files via upload

---
+++ assignment4.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundColor="red";
+
+document.body.style.fontFamily="sans-serif";
+
+document.getElementById('nickname').innerHTML="Harry";
+
+document.getElementById('favorites').innerHTML="Krispy Kreme Donuts";
+
+document.getElementById('hometown').innerHTML="Flushing, Queens, NY";
+
+document.getElementsByTagName('h1')[0].style.backgroundColor="blue";
